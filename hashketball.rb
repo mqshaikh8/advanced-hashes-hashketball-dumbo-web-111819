f8c9867ce31e78f5c62540d78900c2fd43ada43e
Mohammed shaikh <mqshaikh8@gmail.com>
2019-11-06 13:17:21 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,37 @@
-# Write your code here!
+def game_hash
+  {
+    :home =>
+    {
+      :team_name => 
+      {
+        "Brooklyn Nets"
+      }
+      :colors => 
+      {
+        ["Black", "White"]
+      }
+      :players =>
+      {
+        
+      }
+    }
+    :away =>
+    {
+      :team_name => 
+      {
+        
+      }
+      :colors => 
+      {
+        
+      }
+      :players =>
+      {
+        
+      }
+    }
+  }
+end
 
 
 
